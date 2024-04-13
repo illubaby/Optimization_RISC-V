0bad6e9966b42b31f8a488d288333b0b75881bfc
Tran Anh Kiet <anhkiettran.ce@gmail.com>
2024-04-13 18:42:36 +0700

    update

---
+++ Implementation/README_INSTALL.md
@@ -0,0 +1,1 @@
+https://docs.amd.com/r/en-US/ug973-vivado-release-notes-install-license/Release-Notes
---
+++ Implementation/setup.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/Digilent/vivado-boards.git
+
+sudo cp -r vivado-boards/new/board_files/*
+/tools/Xilinx/Vivado/2023.2/data/boards/board_files/
+
